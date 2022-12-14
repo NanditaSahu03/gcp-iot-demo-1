a201bd10ffda3eaf13428f814214cabd125f4026
Nandita Sahu <22912146+NanditaSahu03@users.noreply.github.com>
2022-12-14 17:03:06 +0530

    Update publisher.js

--- publisher.js
+++ publisher.js
@@ -91,7 +91,7 @@ const publishAsync = (
     const payload = '{'+
                     '"registry":"'+argv.registryId +
                     '","deviceID":"'+argv.deviceId +
-                    '","deviceName":"dragline2"' +
+                    '","deviceName":"'+argv.deviceName +
                     ',"pressure":'+Math.random() +
                     ',"speed":'+Math.floor(Math.random() * Math.floor(10)) +
                     ',"engineUpTime":'+Math.floor(Math.random() * Math.floor(10)) +
